54308e84d216a335649104373bc24bffe375d95d
Mark Mikelonis <mark.mikelonis@gmail.com>
2018-02-18 16:10:06 -0800

    all logic working

--- index.js
+++ index.js
@@ -12,20 +12,13 @@ var words = fs.readFileSync("words.txt", "utf8", function(err, data){
 wordsArr = words.split("\n");
 var random = Math.floor(Math.random()* wordsArr.length -1);
 var str = wordsArr[random];
-console.log(str);
+console.log(chalk.blue(str));
 var charArray = str.split("");
-var letters = [];
+// var letters = [];
 var currWord = new Word(charArray);
 
 currWord.guessChar("n");
 var showWord = currWord.display();
-
-for (var c in currWord){
-    
-   console.log(currWord[c]);
-   
-}
-
 
 
 console.log(chalk.blue(showWord));
